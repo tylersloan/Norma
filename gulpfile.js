7d6d504013a7898d061ca976b2e1d796908587d3
jbaxleyiii <james@whitefloordesign.com>
2014-10-07 08:20:07 -0400

    updated package dependencies

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-// Small wrapper function for using gulp with coffeescript
-require('coffee-script/register');
-var path = require('path')
-
-// Dynamically load all gulp tasks
-var requireDir = require('require-dir');
-var dir        = requireDir('./plugins');
